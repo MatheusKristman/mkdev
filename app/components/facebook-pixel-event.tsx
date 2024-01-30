51e1681b9c14d9db427586ac61c9b179e3d89c65
MatheusKristman <kristman058@gmail.com>
2024-01-29 22:02:16 -0300

    arrumado para a versão anterior

--- app/components/facebook-pixel-event.tsx
+++ app/components/facebook-pixel-event.tsx
@@ -1,19 +1,22 @@
 "use client";
 
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
-import ReactPixel from "react-facebook-pixel";
 
 export const FacebookPixelEvents = () => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    ReactPixel.init(process.env.FACEBOOK_PIXEL_ID!, undefined, {
-      autoConfig: true,
-      debug: true,
-    });
-    ReactPixel.pageView();
+    import("react-facebook-pixel")
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init(process.env.FACEBOOK_PIXEL_ID!, undefined, {
+          autoConfig: true,
+          debug: true,
+        });
+        ReactPixel.pageView();
+      });
   }, [pathname, searchParams]);
 
   return null;
