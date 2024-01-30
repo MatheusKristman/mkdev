0ba6a786d68dfd0410c463618585349a43f14ff5
MatheusKristman <kristman058@gmail.com>
2024-01-29 21:58:37 -0300

    teste no arquivo do pixel do facebook

--- app/components/facebook-pixel-event.tsx
+++ app/components/facebook-pixel-event.tsx
@@ -1,19 +1,19 @@
 "use client";
 
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
+import ReactPixel from "react-facebook-pixel";
 
 export const FacebookPixelEvents = () => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    import("react-facebook-pixel")
-      .then((x) => x.default)
-      .then((ReactPixel) => {
-        ReactPixel.init(process.env.FACEBOOK_PIXEL_ID!);
-        ReactPixel.pageView();
-      });
+    ReactPixel.init(process.env.FACEBOOK_PIXEL_ID!, undefined, {
+      autoConfig: true,
+      debug: true,
+    });
+    ReactPixel.pageView();
   }, [pathname, searchParams]);
 
   return null;
