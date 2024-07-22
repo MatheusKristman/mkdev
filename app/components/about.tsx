c7b6f4fd01382b1b735d2adb82ba1e3531a4d52e
MatheusKristman <kristman058@gmail.com>
2024-07-22 18:17:57 -0300

    added about grid and added some details on about text

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -14,31 +14,38 @@ import {
 
 export const About = () => {
   return (
-    <section className="w-full px-6 mb-12 flex flex-col md:px-16 md:mb-24 md:gap-y-12 lg:mb-36 lg:container lg:mx-auto lg:flex-row lg:justify-between lg:items-center">
+    <section className="relative w-full px-6 py-12 flex flex-col md:px-16 md:py-24 md:mb-24 md:gap-y-12 lg:py-36 lg:container lg:mx-auto lg:flex-row lg:justify-center lg:items-center">
+      <Image
+        src="/images/about-grid.png"
+        alt="Conectando Marcas ao Mundo Digital."
+        fill
+        className="absolute top-0 left-0 w-full h-full hidden md:block"
+      />
+
       <motion.div
         initial="initial"
         whileInView="animate"
         viewport={{ once: true }}
         variants={infoContainerAnimation}
-        className="w-full bg-gradient-to-br from-[#155FDA] to-[#6E37DE] rounded-3xl p-12 max-w-3xl mx-auto"
+        className="relative z-10 w-full bg-gradient-to-br from-[#155FDA] to-[#6E37DE] rounded-3xl p-12 max-w-3xl mx-auto"
       >
         <div className="flex flex-col gap-y-6">
           <motion.h2
             variants={textAnimation}
-            className="text-2xl text-light-primary font-bold md:text-4xl"
+            className="text-2xl text-light-primary font-bold text-center md:text-4xl"
           >
             Conectando Marcas ao Mundo Digital.
           </motion.h2>
 
           <motion.p
             variants={textAnimation}
-            className="text-lg font-medium text-light-primary leading-[27px]"
+            className="text-lg font-medium text-light-primary leading-[27px] text-center"
           >
-            Somos impulsionados pela paixão em cada projeto que abraçamos. Nosso
-            compromisso vai além de simplesmente alcançar objetivos; é moldar
-            uma narrativa digital única que captura a essência exclusiva de cada
-            marca. Aqui, reconhecemos que cada cliente é único, e é por isso que
-            oferecemos um atendimento personalizado, dedicado a compreender suas
+            Somos impulsionados pela paixão em cada projeto que abraçamos. Nosso{" "}
+            <strong>compromisso </strong>vai além de simplesmente alcançar
+            objetivos. Aqui, reconhecemos que cada cliente é{" "}
+            <strong>único</strong>, e é por isso que oferecemos um atendimento{" "}
+            <strong>personalizado</strong>, dedicado a compreender suas
             necessidades específicas. Descubra como nossa paixão pela excelência
             digital pode levar sua presença online a novos horizontes.
           </motion.p>
